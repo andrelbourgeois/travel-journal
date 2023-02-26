e17a864ec2ed567cc8e245b4da71fda405534fa9
André Bourgeois <andrelbourgeois@gmail.com>
2023-02-26 13:01:21 +0000

    progress on card structure

--- src/Card.js
+++ src/Card.js
@@ -4,6 +4,18 @@ function Card(props) {
             <div className='Img-box'>
                 <img className="Card-img" src={require('./bordeaux.jpg')} alt="card image"/>
             </div>
+            <div className='Content-box'>
+                <div className='Content-top'>
+                    <img className="Location-icon" src={require('./location.png')} alt="location icon"/>
+                    <h5 className='Card-country'>France</h5>
+                    <a className='Card-location' href=''>View on Google Map</a>
+                </div>
+                <h3 className='Card-place'>
+                    Bordeaux
+                </h3>
+                <strong><p className='Card-date'>1 September 2019 - 22 December 2019</p></strong>
+                <p className='Card-story'></p>
+            </div>
             
         </div>
     )
