9d0131f12c528378095d0f049fb84becb1983e68
André Bourgeois <andrelbourgeois@gmail.com>
2023-02-28 21:50:32 +0000

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,14 +1,28 @@
 travels [
-    { location: {
-        country: "",
-        place: "",
-        map: ""
-    },
-    img: "",
-    date: "",
-    story: ""
+    {
+        id: 0,
+        location: {
+            country: "France",
+            place: "Bordeaux",
+            map: "insert map link"
+        },
+        img: "insert image link",
+        date: "7 September 2019 - 22 December 2019",
+        story: "Something about Bordeaux"
 
-    }
+    },
+    {
+        id: 0,
+        location: {
+            country: "",
+            place: "",
+            map: ""
+        },
+        img: "",
+        date: "",
+        story: ""
+    
+    },
 ]
 
 export default travels
